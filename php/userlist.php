9a13695f5614b4980e06797e148468cec8688c04
codedim <codedim@gmail.com>
2016-10-13 01:27:14 +0300

    Added: UserList items

---
+++ php/userlist.php
@@ -0,0 +1,15 @@
+
+<div id="CurrentRoom" class="UserList">
+
+</div> <!-- current room -->
+
+
+<div id="GeneralRoom" class="UserList">
+	<div class="UserBlock">
+		<div class="Status"> </div>
+		<div class="UserAvatar"> </div>
+			<span class="UserName">UserName</span><br>
+			<span class="UserStatus">Some words about user</span>
+		<div class="UserAction">. </div>
+	</div>
+</div> <!-- general room -->
